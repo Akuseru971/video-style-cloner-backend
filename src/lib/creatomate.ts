9935583447c183f65839ed61d716dffa7207ca0b
Akuseru971 <axellvalentino@gmail.com>
2025-12-06 01:12:32 +0000

    Remove old unused files (creatomate, gcp, storage, old workers)

--- src/lib/creatomate.ts
+++
@@ -1,7 +0,0 @@
-import Creatomate from 'creatomate';
-
-const client = new Creatomate.Client({
-  apiKey: process.env.CREATOMATE_API_KEY || '',
-});
-
-export default client;
--- src/lib/gcpVideo.ts
+++
@@ -1,8 +0,0 @@
-import videoIntelligence from '@google-cloud/video-intelligence';
-
-const client = new videoIntelligence.VideoIntelligenceServiceClient({
-  projectId: process.env.GCP_PROJECT_ID,
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-});
-
-export default client;
