e9f9a9980bed093503815e510a6589056baf0b38
Akuseru971 <axellvalentino@gmail.com>
2025-12-06 02:34:53 +0000

    Add Redis password configuration

--- src/lib/queues.ts
+++ src/lib/queues.ts
@@ -4,6 +4,7 @@ const connection = {
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT || '6379'),
   maxRetriesPerRequest: null,
+    password: process.env.REDIS_PASSWORD,
 };
 
 export const videoAnalysisQueue = new Queue('VIDEO_ANALYSIS', { connection });
